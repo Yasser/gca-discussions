67e7cb99a7ab015aa8c97a0c737e1ed08622f4e4
Yasser Dahab <y@yasserdahab.com>
2014-12-04 09:13:46 -0700

    User sync

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,42 @@ class UsersController < ApplicationController
     end
   end
   
+  def sync
+    request = GcaSsoApi.new("/api/users/groups")
+    response = request.get
+    
+    if response.status == 200
+      access_groups = JSON.parse request.response.body
+      access_groups["groups"].each do |group|
+        AccessGroup.create(key: group["key"], title: group["title"], group_key: group["group"], group_title: access_groups["categories"].select{|c| c["key"] == group["group"]}.first["title"]) if !AccessGroup.where(key: group["key"]).exists?
+      end
+    end
+
+    request = GcaSsoApi.new("/api/users/")
+    response = request.get
+  
+    if response.status == 200
+      users = response.parsed
+      users.each do |user|
+        u = User.where(uid: user["uid"])
+        if u.exists?
+          u = u.first
+          attributes = {}
+          attributes.merge!(access_group_ids: AccessGroup.where(key: user["user_groups"]).pluck(:id))
+          [:first_name, :last_name, :title].each do |attribute|
+            attributes.merge!(attribute => user[attribute.to_s]) if u.send(attribute) != user[attribute.to_s]
+          end
+          u.assign_attributes(attributes)
+        else
+          u = User.new(uid: user["uid"], access_group_ids: AccessGroup.where(key: user["user_groups"]).pluck(:id), email: user["email"], first_name: user['first_name'], last_name: user['last_name'], title: user['title'])
+        end
+        u.save if u.changed? && user["user_groups"].include?("shareholder")
+      end
+      Rails.cache.write(:user_list_updated_at, Time.now)
+    end
+    
+    redirect_to users_url
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
   get '/signout' => 'sessions#destroy'
   get '/auth/failure' => 'sessions#failure'
   
+  resources :users, only: [:index, :destroy] do
+    collection do
+      patch 'sync', to: 'users#sync', as: :sync
+    end
+  end
+  
   root :to => "topics#index", constraints: lambda{|request| request.session[:user].present? }
   root to: 'sessions#index', as: :visitor_root
 end
