f5f32d3e00aa9bff5abecf0c13bc12c34aecd404
Yasser Dahab <y@yasserdahab.com>
2014-12-04 10:14:24 -0700

    Closed topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203034642) do
+ActiveRecord::Schema.define(version: 20141204171059) do
 
   create_table "access_groups", force: true do |t|
     t.string   "key"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20141203034642) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "closed"
   end
 
   add_index "topics", ["user_id"], name: "index_topics_on_user_id"
