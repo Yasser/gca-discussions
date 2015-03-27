a8350d8c68ffde0b0775d6e1e544bb18f383cf3e
Yasser Dahab <y@yasserdahab.com>
2015-03-27 15:02:23 -0600

    Rely on gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,6 @@ class SessionsController < GcaSsoClient::SessionsController
   skip_before_action :authenticate_user!
   skip_before_action :validate_session
 
-  def index
-    redirect_to :signin
-  end
-  
   protected
   
   def permitted_roles
