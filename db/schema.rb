c630484d4486c9f5fe5fc643b65965f7b70a9222
Yasser Dahab <y@yasserdahab.com>
2015-03-27 15:17:53 -0600

    Minor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204171059) do
+ActiveRecord::Schema.define(version: 20150304213737) do
 
   create_table "access_groups", force: true do |t|
     t.string   "key"
